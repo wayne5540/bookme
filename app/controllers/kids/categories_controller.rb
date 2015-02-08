0ba7c95023e3b37f24c0156dc4e30dcea4ece55f
Wayne <wayne@spoonrocket.com>
2015-02-08 00:13:42 -0800

    add category index

--- app/controllers/kids/categories_controller.rb
+++ app/controllers/kids/categories_controller.rb
@@ -1,4 +1,13 @@
 class Kids::CategoriesController < KidsBaseController
   def index
+    @categories = Category.all
+  end
+
+  def create
+    book_list_ids = params[:book][:book_list_ids]
+    book_list_ids.each do |book_list_id|
+      UserCategory.create({user_id: current_user.id, book_list_id: book_list_id})
+    end
+    redirect_to kids_levels_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   namespace :kids do
     root "book_lists#index"
 
-    resources :categories
+    resources :categories do
+      collection do
+        post :add_to_kids
+      end
+    end
     resources :book_lists
     resources :books do
       member do
